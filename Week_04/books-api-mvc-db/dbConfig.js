01cea307bcdf5a40f741589a289bad243df620f7
COMET-C275 <S1024>
2024-05-09 09:48:17 +0800

    Fixed the Usernam and password problem

--- Week_04/books-api-mvc-db/dbConfig.js
+++ Week_04/books-api-mvc-db/dbConfig.js
@@ -1,8 +1,8 @@
 // Step 4
 // Part 1 - Add several lines of code
 module.exports = {
-    user: "username", // Replace with your SQL Server login username
-    password: "password", // Replace with your SQL Server login password
+    user: "booksapi", // Replace with your SQL Server login username
+    password: "Password", // Replace with your SQL Server login password
     server: "localhost",
     database: "bed_db",
     trustServerCertificate: true,
